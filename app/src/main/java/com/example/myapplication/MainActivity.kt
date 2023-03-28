85c64979ddfcfd668219540b2536af8c421d5e51
TFstorm22 <1376280@senati.pe>
2023-03-28 02:31:38 -0500

    añadi layouts de tareas

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    fun openNewTask(view: View) {
+        startActivity(Intent(this, TaskActivity::class.java))
     }
 }
